5e1f3b50d0dabacb0ec16602d35b34f15922b2dd
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-05-16 15:54:42 -0500

    Se crear rutaprincipal en configuración de servidor para gestionar  endpoints de api restful, igual se crea mockup para generar pruebas con postman

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const path = require('path');
 const app = express();
 
 const productsRouter = require('./routes/products');
+const productsApiRouter = require('./routes/api/products.js')
+
+// el primer parámetro es un alias por defautl 'static' se va agregar a la ruta 
+// donde se encuentran los assets cundo importe el css en el head del pug layout
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use('/products', productsRouter);
+//
+app.use("/api/products", productsApiRouter);
 
 const server = app.listen(3000, () => {
   console.log(`Listen to port ${server.address().port} !!`)
--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const products = [
-  {
-    name: 'red shoes',
-    price: 75
-  },
-  {
-    name: 'black bike',
-    price: 300
-  }
-];
+const products = require('../utils/mocks/products');
 
 router.get('/', function(req, res) {
   res.render('products', { products });
